0139603d85af6fbc06a11a8fb9ae8510030a77ce
m1amgn <59062076+m1amgn@users.noreply.github.com>
2024-11-16 20:59:28 +0300

    Update generate_qr_code.py

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -17,3 +17,24 @@ qr.svg('qr.svg', scale=6)
 
 # save in png with low error correction
 qr.png('qr_low.png', scale=6, module_color='red', background='white', quiet_zone=1)
+
+
+#or
+import qrcode
+
+input_URL = "https://www.google.com/"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+qr.add_data(input_URL)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="red", back_color="white")
+img.save("url_qrcode.png")
+
+print(qr.data_list)
